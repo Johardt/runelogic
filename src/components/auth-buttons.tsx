61a88ec84cc094ad0bc1aa0ffc1f6d7a6d8db68a
Joel Eckhardt <495223@student.fontys.nl>
2025-04-12 19:15:09 +0200

    Update packages, auth buttons

--- src/components/auth-buttons.tsx
+++ src/components/auth-buttons.tsx
@@ -3,16 +3,9 @@ import { User, LogOut, Mail } from "@geist-ui/icons";
 
 import Link from "next/link";
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "./ui/navigation-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { DropdownMenu, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 export async function AuthButtons() {
   const supabase = await createClient();
@@ -33,37 +26,39 @@ export async function AuthButtons() {
     return (
       <div className="flex flex-row items-center space-x-4">
         <Link href="/user/inbox" className="flex items-center">
-          <Mail color="text-neutral-800" size={32} />
+          <Mail color="text-neutral-800" size={28} />
         </Link>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="flex items-center">
-                <Avatar>
-                  <AvatarImage src="user/profile-pic" /> {/* TODO */}
-                  <AvatarFallback>
-                    <User color="black" size={32}></User>
-                  </AvatarFallback>
-                </Avatar>
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <Link href="/profile" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    My Account
-                  </NavigationMenuLink>
-                </Link>
-                <Link href="/auth/logout" legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    <div className="flex items-center">
-                      <LogOut color="black" />
-                      <span className="mx-2">Log out</span>
-                    </div>
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button className="focus:outline-none cursor-pointer">
+              <Avatar>
+                <AvatarImage src="user/profile-pic" /> {/* TODO */}
+                <AvatarFallback>
+                  <User color="black" size={32}></User>
+                </AvatarFallback>
+              </Avatar>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="min-w-[200px] mt-2 p-1 bg-white shadow-md rounded-md">
+            <DropdownMenuItem asChild>
+              <Link
+                href="/profile"
+                className="flex items-center px-3 py-2 rounded hover:bg-gray-100"
+              >
+                My Account
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link
+                href="/auth/logout"
+                className="flex items-center px-3 py-2 rounded hover:bg-gray-100"
+              >
+                <LogOut color="black" />
+                <span className="mx-2">Log out</span>
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     );
   }
